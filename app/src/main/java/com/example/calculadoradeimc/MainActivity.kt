bbbb7d36e3582b2c121a71ed0c39f949213a9274
Rodrigo <rodrigorasilva@hotmail.com>
2022-11-17 10:06:38 -0300

    criacao de condicoes conforme os resultados

--- app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadoradeimc
 
+import android.annotation.SuppressLint
 import android.icu.text.DecimalFormat
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,10 @@ class MainActivity : AppCompatActivity() {
         val peso = findViewById<EditText>(R.id.etPeso)
         val botao = findViewById<Button>(R.id.btnCalcular)
         val btnLimpar = findViewById<Button>(R.id.btnLimpar)
-        var resultado = findViewById<TextView>(R.id.tvResultado)
+        val resultado = findViewById<TextView>(R.id.tvResultado)
+        val classificacao = findViewById<TextView>(R.id.tvClassificacao)
 
-        botao.setOnClickListener{
+        botao.setOnClickListener {
             val editAltura = altura.text.toString().toFloat()
             val editPeso = peso.text.toString().toFloat()
 
@@ -31,12 +34,32 @@ class MainActivity : AppCompatActivity() {
             val calculoFormatado = decimal.format(calculo)
 
             resultado.text = calculoFormatado.toString()
+
+            if (calculo <= 18.5) {
+                classificacao.text = "Você está abaixo do peso."
+            }
+            else if (calculo >= 18.6 && calculo <= 24.9){
+                classificacao.text = "Você está no peso ideal, parabéns!"
+            }
+            else if (calculo >= 25.0 && calculo <= 29.9){
+                classificacao.text = "Você está levemente acima do peso."
+            }
+            else if (calculo >= 30.0 && calculo <= 34.9){
+                classificacao.text = "Você está com obesidade grau 1"
+            }
+            else if (calculo >= 35.0 && calculo <= 39.9){
+                classificacao.text = "Você está com obesidade grau 2"
+            }
+            else if (calculo >= 40.0){
+                classificacao.text = "Você está com obesidade grau 3"
+            }
         }
 
         btnLimpar.setOnClickListener{
              altura.text.clear()
              peso.text.clear()
              resultado.text = ""
+             classificacao.text = ""
 
         }
 
